10f26501c37f1a228aa32b712a21e186c69e854f
Ron <rumbletumjum@gmail.com>
2021-05-02 02:47:46 -0400

    change default color scheme

--- config.h
+++ config.h
@@ -95,42 +95,42 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	[0] = "#2d2e2d", /* black   */
+	[1] = "#a36666", /* red     */
+	[2] = "#90a57d", /* green   */
+	[3] = "#d7ad87", /* yellow  */
+	[4] = "#59829c", /* blue    */
+	[5] = "#c79ec4", /* magenta */
+	[6] = "#80c2a2", /* cyan    */
+	[7] = "#d0d0d0", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#4a4845", /* black   */
+	[9]  = "#cc6666", /* red     */
+	[10] = "#acb36d", /* green   */
+	[11] = "#f0c674", /* yellow  */
+	[12] = "#81a2be", /* blue    */
+	[13] = "#b294bb", /* magenta */
+	[14] = "#a3dad3", /* cyan    */
+	[15] = "#efefef", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/* special colors */
+	[256] = "#252525", /* background */
+	[257] = "#f0f0f0", /* foreground */
+	[258] = "#6699cc",      /* cursor */
+	[259] = "#555555",      /* reversecursor */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
